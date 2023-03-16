e9d0fa8cd26c66a57c1f9115047b15ff2a2b2158
Softwarechamp <softwarechamp@brainupgrade.in>
2023-03-16 22:26:23 +0530

    refine

---
+++ src/main/java/in/softwarechamp/skps0910/p2.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class p2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter 2 numbers \'m\' and \'n\' such that m<n");
+		System.out.print("m= ");
+		long m = sc.nextLong();
+		System.out.print("n= ");
+		long n = sc.nextLong();
+
+		boolean bo = false;
+		long j;
+		if (m < n) {
+			System.out.println("Numbers between " + m + " and " + n +
+					" without any zeroes are --");
+			m++;
+			for (; m < n;) {
+				j = m;
+
+				for (; j > 0; j = j / 10) {
+					if (j % 10 != 0) {
+						bo = true;
+					} else {
+						bo = false;
+						break;
+					}
+				}
+				if (bo == true) {
+					System.out.print(m + " ");
+
+				}
+				m++;
+			}
+
+		} else
+			System.out.println("incorrect input");
+	}
+}
